55fe4ecc066b384c7cd0a2157bd224666a9d064b
Fernando Morán Fougerat <fermofou@gmail.com>
2023-09-23 15:51:32 -0600

    Update getTranscript.py

--- PythonFiles/getTranscript.py
+++ PythonFiles/getTranscript.py
@@ -1,1 +1,77 @@
-#hola
+import os
+from softtek_llm.chatbot import Chatbot
+from softtek_llm.models import OpenAI
+from softtek_llm.cache import Cache
+from softtek_llm.vectorStores import PineconeVectorStore
+from softtek_llm.embeddings import OpenAIEmbeddings
+from softtek_llm.schemas import Filter
+from dotenv import load_dotenv
+#importacion
+
+load_dotenv()
+OPENAI_API_KEY = "6b25369971534252bbcee5e488ce59f1"
+if OPENAI_API_KEY is None:
+    raise ValueError("OPENAI_API_KEY not found in .env file")
+
+OPENAI_API_BASE = "https://openaistkinno.openai.azure.com/"
+if OPENAI_API_BASE is None:
+    raise ValueError("OPENAI_API_BASE not found in .env file")
+
+OPENAI_EMBEDDINGS_MODEL_NAME = "OpenAIEmbeddings"
+if OPENAI_EMBEDDINGS_MODEL_NAME is None:
+    raise ValueError("OPENAI_EMBEDDINGS_MODEL_NAME not found in .env file")
+
+OPENAI_CHAT_MODEL_NAME = "InnovationGPT2"
+if OPENAI_CHAT_MODEL_NAME is None:
+    raise ValueError("OPENAI_CHAT_MODEL_NAME not found in .env file")
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+if PINECONE_API_KEY is None:
+    raise ValueError("PINECONE_API_KEY not found in .env file")
+
+*PINECONE_ENVIRONMENT = os.getenv("PINECONE_ENVIRONMENT")
+if PINECONE_ENVIRONMENT is None:
+    raise ValueError("PINECONE_ENVIRONMENT not found in .env file")
+
+PINECONE_INDEX_NAME = os.getenv("PINECONE_INDEX_NAME")
+if PINECONE_INDEX_NAME is None:
+    raise ValueError("PINECONE_INDEX_NAME not found in .env file")
+vector_store = PineconeVectorStore(
+    api_key=PINECONE_API_KEY,
+    environment=PINECONE_ENVIRONMENT,
+    index_name=PINECONE_INDEX_NAME,
+)
+embeddings_model = OpenAIEmbeddings(
+    api_key=OPENAI_API_KEY,
+    model_name=OPENAI_EMBEDDINGS_MODEL_NAME,
+    api_type="azure",
+    api_base=OPENAI_API_BASE,
+)
+cache = Cache(
+    vector_store=vector_store,
+    embeddings_model=embeddings_model,
+)
+model = OpenAI(
+    api_key=OPENAI_API_KEY,
+    model_name=OPENAI_CHAT_MODEL_NAME,
+    api_type="azure",
+    api_base=OPENAI_API_BASE,
+    verbose=True,
+)
+filters = [
+    Filter(
+        type="DENY",
+        case="ANYTHING about emotions. YOU CANNOT talk about emotions AT ALL.Say ONLY facts.",
+    )
+]
+chatbot = Chatbot(
+    model=model,
+    description="You are a very helpful and polite chatbot",
+    filters=filters,
+    cache=cache,
+    verbose=True,
+)
+response = chatbot.chat(
+    "Hello! My name is Jeff.",
+    print_cache_score=True,
+    cache_kwargs={"namespace": "chatbot-test"},
+)
