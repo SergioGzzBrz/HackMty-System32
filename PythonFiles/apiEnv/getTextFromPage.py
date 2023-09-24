24eb278f9e1fde1faab3edbf7d099206e6abd366
Fernando Morán Fougerat <fermofou@gmail.com>
2023-09-24 08:36:31 -0600

    Update getTextFromPage.py

--- PythonFiles/apiEnv/getTextFromPage.py
+++ PythonFiles/apiEnv/getTextFromPage.py
@@ -1,3 +1,4 @@
+'''
 import scrapy
 from scrapy.crawler import CrawlerProcess
 from bs4 import BeautifulSoup
@@ -73,22 +74,37 @@ def getTheText(text):
     return final[300:min(5000, len(final) - 1)]
 
 
+if _name_ == "_main_":
+    extracted = getTheText("https://es.wikipedia.org/wiki/Donald_Trump")
+    print(extracted)
+'''
+
+import requests
+from bs4 import BeautifulSoup
+
+final_text = ""
+long_paragraphs = []
+
+def get_post_text(url):
+    global final_text
+    global long_paragraphs
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        paragraphs = soup.find_all('p')
+        for paragraph in paragraphs:
+            text = paragraph.get_text()
+            if len(text) > 100:
+                long_paragraphs.append(text)
+        final_text = " ".join(long_paragraphs)
+    else:
+        print("Error: Unable to fetch data from the provided URL.")
+
+def getTheText(url):
+    global final_text
+    get_post_text(url)
+    return final_text[300:min(5000, len(final_text) - 1)]
+
 if __name__ == "__main__":
     extracted = getTheText("https://es.wikipedia.org/wiki/Donald_Trump")
-    # https://www.genbeta.com/actualidad/proyecto-gutenberg-tiene-5-000-audiolibros-nuevos-que-puedes-escuchar-gratis-ha-hecho-inteligencia-artificial
-    # extracted = getTheText("https://www.genbeta.com/actualidad/proyecto-gutenberg-tiene-5-000-audiolibros-nuevos-que-puedes-escuchar-gratis-ha-hecho-inteligencia-artificial")
-    # print(extracted)
-    # print(len(extracted))
-    # '''print("tamano final de extracted", len(extracted))
-    # # print(paragraphs[0])
-    # print("tamano de parrafos ddd final",len(paragraphs))'''
-    # '''for i in range(len(paragraphs)):
-    #     print(len(paragraphs[i]))'''
-
-    # '''
-    # print("tamano de parrafos ddd final 2", len(long_paragraphs))
-
-    # for i in range(len(long_paragraphs)):
-    #     print(long_paragraphs[i])
-
-    # # print(paragraphs[2])'''
+    print(extracted)
